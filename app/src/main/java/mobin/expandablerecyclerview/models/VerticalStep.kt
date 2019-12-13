d8856c64fa87403f270760576eb37c99f49c4aed
Mobin Munir <mmobinbutt@gmail.com>
2019-12-13 05:44:59 +0500

    master stable.

--- app/src/main/java/mobin/expandablerecyclerview/models/VerticalStep.kt
+++
@@ -1,8 +0,0 @@
-package mobin.expandablerecyclerview.models
-
-data class VerticalStep(val title: String, val summary: String) {
-
-    var btnText1: String? = null
-    var btnText2: String? = null
-    var isSelected = false
-}
